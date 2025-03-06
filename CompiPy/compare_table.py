8d466c98b8216d2ed800b063e1d36d49800e3f90
Bekian <Bekian.Cat@gmail.com>
2025-03-06 16:52:51 -0600

    feat(app): improved change detection algorithm
    see changes

--- CompiPy/compare_table.py
+++ CompiPy/compare_table.py
@@ -4,19 +4,20 @@ df2 = pd.read_csv('output2.csv')
 
 
 def compare_table(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-    value_cols = [col for col in df1.columns if col != 'Name']
+    primary_key = df1.columns[0]
+    value_cols = [col for col in df1.columns if col != primary_key]
+
     inner_merged = pd.DataFrame.merge(
-        df1, df2, on='Name', suffixes=('_new', '_old'))
+        df1, df2, on=primary_key, suffixes=('_new', '_old'))
+
     changed_mask = pd.Series(False, index=inner_merged.index)
     for col in value_cols:
-        changed_mask |= inner_merged[f'{col}_old'].fillna(
-            '') != inner_merged[f'{col}_new'].fillna('')
+        old_col = f'{col}_old'
+        new_col = f'{col}_new'
+        if old_col in inner_merged and new_col in inner_merged:
+            changed_mask |= inner_merged[old_col] != inner_merged[new_col]
 
-    changed_rows = inner_merged.loc[changed_mask,
-                                    ['Name'] + sum([[f'{col}_new', f'{col}_old']
-                                                   for col in value_cols], [])
-                                    ]
-
+    changed_rows = inner_merged[changed_mask]
     print(changed_rows)
     return changed_rows
 
